65dfe6c28a580ea33a2b1058c2ea551f6c475936
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-11-16 22:33:07 -0500

    update-automated-save

--- test-fixtures/activeIncident/activeincident_test.go
+++ test-fixtures/activeIncident/activeincident_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestActiveJSON(t *testing.T) {
 	result := ActiveJSON()
-	if !strings.Contains(string(result),"9252418") {
+	if !strings.Contains(string(result), "9252418") {
 		t.Fatalf("fail.. expected eid ")
 
 	}
